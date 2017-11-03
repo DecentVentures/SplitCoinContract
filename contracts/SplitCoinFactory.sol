c9590f684e7c8eb6a7641e989e220fdc9f45b1bc
Micah Riggan <micahriggan@gmail.com>
2017-11-02 22:00:33 -0500

    Removing build directory

--- contracts/SplitCoinFactory.sol
+++ contracts/SplitCoinFactory.sol
@@ -3,9 +3,13 @@ import "./SplitCoin.sol";
 
 contract SplitCoinFactory {
 	mapping(address => address[]) public contracts;
+  event Deployed (
+    address _deployed
+  );
 	function make(address[] users, uint[] ppms, address refer) public returns (address) {
 		SplitCoin sc = new SplitCoin(users, ppms, refer);
 		contracts[msg.sender].push(sc);
+    Deployed(sc);
 		return sc;
 	}
 }
--- truffle.js
+++ truffle.js
@@ -4,6 +4,11 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    staging: {
+      host: "localhost",
+      port: 8545,
+      network_id: "3"
     }
   }
 };
